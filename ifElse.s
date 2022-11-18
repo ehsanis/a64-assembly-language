cf25b4a856502f6ed6a96748e44211968d1f414a
Sharif Ehsani <47335894+sharifehsani@users.noreply.github.com>
2022-11-18 13:59:44 -0700

    Update ifElse.s

--- ifElse.s
+++ ifElse.s
@@ -1,6 +1,6 @@
 // Author: Sharif Ehsani
 // Date: Dec 13, 2019
-// www.github.com/ehsanis
+// https://github.com/sharifehsani
 
 // This program implements simple if, else if and else construct in assembly language using
 // macros for ARMv8 architercture.
