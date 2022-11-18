f2af5087ab65fae3261261a0e4bbef8654108815
Sharif Ehsani <47335894+sharifehsani@users.noreply.github.com>
2022-11-18 13:58:39 -0700

    Update HelloWorld.s

--- HelloWorld.s
+++ HelloWorld.s
@@ -2,7 +2,7 @@
 * Author: Sharif Ehsani
 * Date: Dec 14, 2019
 * www.gitbuh.com/ehsanis
-* https://sharifehsani.github.io/
+* https://github.com/sharifehsani
 *
 * This program runs on ARMv8 archeticture. assembly language 
 * it prints 2 lines of statements with the first one having a decimal value
